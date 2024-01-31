e8b1898af52dfb5d55ff299d64adc2c0a52d97c5
Veronica <aleshauskas@gmail.com>
2024-01-31 15:59:03 +0300

    Lab1-Iris

--- lab1.py
+++ lab1.py
@@ -3,6 +3,8 @@ import mglearn
 import pandas as pds
 from sklearn.datasets import load_iris
 
+# Основы интеллектуального анализа данных. Лаб. 1
+
 iris_dataset = load_iris()
 print("Ключи iris_dataset: \n{}".format(iris_dataset.keys()))
 print(iris_dataset['DESCR'][:193] + "\n...")
@@ -15,14 +17,16 @@ print("Тип массива target: {}".format(type(iris_dataset['target'])))
 print("Форма массива target: {}".format(iris_dataset['target'].shape))
 print("Ответы:\n{}".format(iris_dataset['target']))
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(
-iris_dataset['data'], iris_dataset['target'], random_state=0)
+    iris_dataset['data'], iris_dataset['target'], random_state=0)
 print("форма массива X_train: {}".format(X_train.shape))
 print("форма массива y_train: {}".format(y_train.shape))
 print("форма массива X_test: {}".format(X_test.shape))
 print("форма массива y_test: {}".format(y_test.shape))
 iris_dataframe = pds.DataFrame(X_train, columns=iris_dataset.feature_names)
 from pandas.plotting import scatter_matrix
+
 grr = scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
-hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+                     hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
 plt.show()
